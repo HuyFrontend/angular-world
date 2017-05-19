7b19edf1c371f44ba27654cfe64f217fd4e0f8e0
Huy Xuan Vo <huyxuan.vo@bluecomgroup.com>
2017-05-19 18:21:02 +0700

    click event

--- app/course.component.ts
+++ app/course.component.ts
@@ -1,7 +1,7 @@
 import {Component} from 'angular2/core';
-import {CourseService} from './course.service'
-import {AutoGrowDirective} from './auto-grow.directive'
-
+import {CourseService} from './course.service';
+import {AutoGrowDirective} from './auto-grow.directive';
+import {NewCourseDirective} from './add-new-course.directive';
 
 @Component({
   selector: 'courses',
@@ -9,6 +9,7 @@ import {AutoGrowDirective} from './auto-grow.directive'
     <h2>Courses</h2>
     {{ title }}
     <input type="text" autoGrow />
+    <button type="text" addNewCourse>Add</button>
     <ul>
       <li *ngFor="#course of courses">
       {{ course }}
@@ -16,15 +17,16 @@ import {AutoGrowDirective} from './auto-grow.directive'
     </ul>
   `,
   providers: [CourseService],
-  directives: [AutoGrowDirective]
+  directives: [AutoGrowDirective, NewCourseDirective]
 })
 
 export class CoursesComponent {
-  title = 'Course Block';
+  title = 'New course';
   //courses = ['1', '2', '3'];
   // use service
   courses;
   constructor (courseService: CourseService) {
     this.courses = courseService.getCourses();
   }
+  //fruits = ['Apple', 'Orange'];
 }
