7a96e8c9e6355de1aacd9c229b1ee710f496c7e2
Huy Xuan <huyvoxuan8489@gmail.com>
2017-05-23 22:04:48 +0700

    add employee

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import {Observable} from 'rxjs/Rx';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { EmployeeService } from './employee/employee.service';
+import { HomeComponent} from './home/home.component';
+
+import { AppRoutes } from './app.routes';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [ AppComponent, EmployeeComponent, HomeComponent ],
+  imports: [ BrowserModule, FormsModule, HttpModule, AppRoutes ],
+  providers: [ EmployeeService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
