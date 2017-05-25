298890c693cd4d74315ab224c14004ee2f8c30a7
Huy Xuan <huyvoxuan8489@gmail.com>
2017-05-26 00:54:24 +0700

    update form

--- src/app/forms/hero-form.component.ts
+++ src/app/forms/hero-form.component.ts
@@ -9,10 +9,21 @@ import { Hero }    from './hero';
 })
 export class HeroFormComponent {
   powers = [ 'Option 1', 'Option 2', 'Option 3', 'Option 4' ];
-  model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+  model = new Hero(18, 'Huy Vo', this.powers[0], 'Chuck Overstreet');
+  //model = new Hero(18, 'Dr IQ', this.powers[0]);
   submitted = false;
-  onSubmit() { this.submitted = true; }
+  onSubmit() {
+    this.submitted = true;
+  }
   newHero() {
     this.model = new Hero(42, '', '');
+  }
+  get diagnostic() {
+    //return JSON.stringify(this.model);
+    return 'vo xuan huy';
+  }
+  get FirstName(): string {
+    console.log("Get FirstName : ", this.model.name);
+    return this.model.name;
   }
 }
