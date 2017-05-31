312f44961a751160b1423428a0aac70dd0aa566f
Huy Xuan <huyvoxuan8489@gmail.com>
2017-05-31 23:35:13 +0700

    update employee detail

--- src/app/employee/employee.detail.component.ts
+++ src/app/employee/employee.detail.component.ts
@@ -12,28 +12,38 @@ export class EmployeeDetailComponent implements OnInit, OnDestroy{
     public id: number;
     public subcription: Subscription;
     public employee: Employee;
-    // @Input() employee: Employee;
+    private initEmployee: Employee = {
+        id: 123,
+        name: 'Huy',
+        age: 30,
+        phone: '0942404202'
+
+    };
+
     constructor(private router: Router, private activated: ActivatedRoute, private employeeService: EmployeeService) {}
     getEmployee(id: number) {
         var that = this;
 
         this.employeeService.getEmployeeObservable(id).subscribe((res: any) => {
             this.employee = res;
-            console.log('this.employee', this.employee);
+            console.log('this employee', this.employee);
         }, error => {
             console.log('API Error');
         });
     }
 
     ngOnInit() {
-        this.subcription = this.activated.params.subscribe(params => {
-            this.id = params['id'];
-            console.log(this.id);
-        })
 
-        this.getEmployee(this.id);
+        this.employee = this.initEmployee;
+
+        // this.subcription = this.activated.params.subscribe(params => {
+        //     this.id = params['id'];
+        //     console.log(this.id);
+        // });
+
+        //this.getEmployee(this.id);
     }
     ngOnDestroy(){
-        this.subcription.unsubscribe();
+        //this.subcription.unsubscribe();
     }
 }
