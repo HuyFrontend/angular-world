94c7d65eb52c4f30a8550f17a21c727d5c69c8f3
Huy Xuan Vo <huyxuan.vo@bluecomgroup.com>
2017-05-31 15:12:42 +0700

    update form

--- src/app/forms/hero-form.component.ts
+++ src/app/forms/hero-form.component.ts
@@ -1,29 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Directive } from '@angular/core';
 import { Hero }    from './hero';
 @Component({
   selector: 'hero-form',
   templateUrl: './hero-form.html',
   styleUrls: [
     './hero-form.css'
-  ]
+  ],
 })
 export class HeroFormComponent {
   powers = [ 'Option 1', 'Option 2', 'Option 3', 'Option 4' ];
-  model = new Hero(18, 'Huy Vo', this.powers[0], 'Chuck Overstreet');
-  //model = new Hero(18, 'Dr IQ', this.powers[0]);
+  hero = new Hero(18, 'Huy Vo', this.powers[0], 'Chuck Overstreet');
+  //hero = new Hero(18, 'Dr IQ', this.powers[0]);
   submitted = false;
   onSubmit() {
     this.submitted = true;
   }
   newHero() {
-    this.model = new Hero(42, '', '');
+    this.hero = new Hero(42, '', '');
   }
   get diagnostic() {
-    //return JSON.stringify(this.model);
+    //return JSON.stringify(this.hero);
     return 'vo xuan huy';
   }
   get FirstName(): string {
-    console.log("Get FirstName : ", this.model.name);
-    return this.model.name;
+    console.log("Get FirstName : ", this.hero.name);
+    return this.hero.name;
   }
 }
