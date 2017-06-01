ab2da9cb017a047d6bc72819a1c74320b7fa9a93
Huy Xuan Vo <huyxuan.vo@bluecomgroup.com>
2017-06-01 17:28:50 +0700

    update

--- src/app/employee/employee-overview.component.ts
+++ src/app/employee/employee-overview.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'employee-overview',
   template: `<h3> This is Overview </h3>
   `
 })
-export class EmployeeOverviewComponent {
+export class EmployeeOverviewComponent implements OnInit, OnDestroy {
+  public parentRouterID: number;
+  public sub: Subscription;
+  constructor(private router: Router, private activated: ActivatedRoute) {}
+  getParentRouteID() {
+    this.sub = this.activated.parent.params.subscribe(params => {
+      this.parentRouterID = params.id;
+      console.log('this.parentRouterID', this.parentRouterID);
+    });
+  }
+
+  ngOnInit() {
+    this.getParentRouteID();
+  }
+  ngOnDestroy(){
+    if (this.sub) {
+      console.log('Unscrible', this.sub);
+      this.sub.unsubscribe();
+    }
+  }
 }
 
